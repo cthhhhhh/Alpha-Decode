5ebbcd371ea6909c628a2c41b0cce73d37c9fc52
ALPHA-DECODE Dev <dev@alpha-decode.app>
2026-02-19 01:18:02 +0800

    Initial commit: ALPHA-DECODE Spring Boot app

---
+++ src/main/java/com/alphadecode/app/AlphaDecodeApplication.java
@@ -0,0 +1,12 @@
+package com.alphadecode.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaDecodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaDecodeApplication.class, args);
+    }
+}
