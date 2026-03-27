5bef608b8503058f8befa393fb07658935cb6826
Lim-JL <limjiale.ljl@gmail.com>
2026-03-27 22:05:59 +0800

    fix:webpage now works as expected when accessed from localhost:8080

--- src/main/java/com/csd/cs203t1/security/SecurityConfig.java
+++ src/main/java/com/csd/cs203t1/security/SecurityConfig.java
@@ -1,21 +1,21 @@
 package com.csd.cs203t1.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -73,7 +73,9 @@ public class SecurityConfig {
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/*.js")).permitAll()
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/*.css")).permitAll()
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/*.ico")).permitAll()
-                        .anyRequest().authenticated())
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/*.svg")).permitAll()
+                        .requestMatchers(request -> !request.getServletPath().startsWith("/api")).permitAll()// Allow all non-API requests (like frontend routes) to be accessed without authentication
+                        .anyRequest().authenticated() )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
